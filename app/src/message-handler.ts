b1a20bb69a50b9ebd5c79905b7922c990c0825bc
John Violano <johnviolano@users.noreply.github.com>
2020-01-05 22:36:13 +0000

    Missed a paren while fixing it live, classic

--- app/src/message-handler.ts
+++ app/src/message-handler.ts
@@ -34,7 +34,7 @@ class DiscordListener {
 
 
     // Command did not start with bot mention, probably not a command
-    if(!firstWord.includes(DiscordClient.user.id) return;
+    if(!firstWord.includes(DiscordClient.user.id)) return;
 
     let cmd = args.shift();
 
